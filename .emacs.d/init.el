0ba350635d3a4d5b5dfd8c7fab97d8c18e193bd2
yuokada <callistoiv+git@gmail.com>
2012-07-17 16:52:47 +0900

    update somefiles

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -256,7 +256,7 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(rst-level-1-face ((t (:background "green" :foreground "LightSkyBlue"))) t)
- '(rst-level-2-face ((t (:foreground "LightGoldenrod"))) t)
+ '(rst-level-2-face ((t (:background "green" :foreground "LightGoldenrod"))) t)
  '(rst-level-3-face ((t (:foreground "Cyan1"))) t)
  '(rst-level-4-face ((t (:foreground "chocolate1"))) t)
  '(rst-level-5-face ((t (:foreground "PaleGreen"))) t)
--- .vimrc
+++ .vimrc
@@ -149,3 +149,5 @@ set t_Co=256
 "colorscheme molokai
 let g:molokai_original = 1
 set background=light
+
+"markdown
